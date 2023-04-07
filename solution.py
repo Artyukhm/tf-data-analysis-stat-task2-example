645cd71b892e8d116a3b27b52c1e14f2f75aa003
Artyukhm <65104515+Artyukhm@users.noreply.github.com>
2023-04-08 01:18:14 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -10,11 +10,10 @@ def solution(p: float, x: np.array) -> tuple:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    mean = np.mean(x)
-    std = np.std(x, ddof=1)
     n = len(x)
+    t = x.mean() / (np.std(x) / np.sqrt(n))
     alpha = 1 - p
-    t_value = t.ppf(1 - alpha/2, n-1)
-    ci_left = mean - t_value * std / np.sqrt(n)
-    ci_right = mean + t_value * std / np.sqrt(n)
-    return ci_left, ci_right
+    df = n - 1
+    lower = scipy.stats.t.ppf(alpha / 2, df)
+    upper = scipy.stats.t.ppf(1 - alpha / 2, df)
+    return (lower, upper)
