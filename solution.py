76b14fecaa7d91a6482d68a0d3326683fa2633e7
Artyukhm <65104515+Artyukhm@users.noreply.github.com>
2023-04-08 01:22:29 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 
-from scipy.stats import norm
+from scipy.stats import norm, chi2
 
 
 chat_id = 1695235986 # Ваш chat ID, не меняйте название переменной
@@ -10,10 +10,11 @@ def solution(p: float, x: np.array) -> tuple:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    n = len(x)
-    t = x.mean() / (np.std(x) / np.sqrt(n))
     alpha = 1 - p
-    df = n - 1
-    lower = scipy.stats.t.ppf(alpha / 2, df)
-    upper = scipy.stats.t.ppf(1 - alpha / 2, df)
-    return (lower, upper)
+    df = 2 * x.shape[0]
+    r2 = np.dot(x, x) ** 0.5
+    z1 = chi2.ppf(alpha/2, df)
+    z2 = chi2.ppf(1-alpha/2, df)
+    left = r2 / (5 * z2**0.5)
+    right = r2 / (5 * z1**0.5)
+    return left, right
